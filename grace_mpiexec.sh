8fe75a5069f34b374eda200ded785b176103f169
Justus <justus.dressler@uni-jena.de>
2024-12-02 13:56:59 +0100

    added git ignore and adjusted number of threads

--- grace_mpiexec.sh
+++ grace_mpiexec.sh
@@ -1,5 +1,7 @@
+# export PATH=/home/justus/Linux_aarch64/24.11/comm_libs/mpi/bin:\$PATH
 PATH=/home/justus/mpich/bin:\$PATH
 NUM_THREADS=144
-HALF_THREADS=$((NUM_THREADS/2))
+COMM_THREADS_PER_PROCESS=3
+THREADS_PER_NODE=$((NUM_THREADS/2-COMM_THREADS_PER_PROCESS))
 
-mpirun -n 2 --bind-to numa --map-by numa -env OMP_NUM_THREADS=$HALF_THREADS ./build/bench_binary_mpi
+mpirun -n 2 --bind-to numa --map-by numa -env OMP_NUM_THREADS=$THREADS_PER_NODE ./build/bench_binary_mpi
