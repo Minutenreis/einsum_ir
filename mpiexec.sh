ef4ca1de7ad9713c8646b4748514f98b3160dbbf
Minutenreis <Minutenreis@gmail.com>
2025-01-18 19:59:13 +0100

    made master worker testable

--- mpiexec.sh
+++ mpiexec.sh
@@ -1,4 +1,4 @@
 NUM_THREADS=20
 HALF_THREADS=$((NUM_THREADS/2))
-mpirun -n 1 -env OMP_NUM_THREADS=$HALF_THREADS -env OMP_PLACES={0:$HALF_THREADS}             ./build/bench_binary_mpi :\
-       -n 1 -env OMP_NUM_THREADS=$HALF_THREADS -env OMP_PLACES={$HALF_THREADS:$HALF_THREADS} ./build/bench_binary_mpi
+mpirun -n 1 -env OMP_NUM_THREADS=$HALF_THREADS -env OMP_PLACES={0:$HALF_THREADS}             ./build/bench_binary_mpi 32 70 :\
+       -n 1 -env OMP_NUM_THREADS=$HALF_THREADS -env OMP_PLACES={$HALF_THREADS:$HALF_THREADS} ./build/bench_binary_mpi 32 70
